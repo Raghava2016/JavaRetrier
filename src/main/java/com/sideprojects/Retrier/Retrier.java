4a14765c07b4957ed3b21544f1a67487a9383753
Raghava2016 <raghava.juvvaji@gmail.com>
2017-05-26 00:12:58 +0530

    Finished retrier project

--- src/main/java/com/sideprojects/Retrier/Retrier.java
+++ src/main/java/com/sideprojects/Retrier/Retrier.java
@@ -17,28 +17,52 @@ import java.util.concurrent.Callable;
 public class Retrier {
 
 	final Predicate<Integer> stopStratgey;
-	
-	private Retrier(Predicate<Integer> stopStratgey) {
+	final Predicate<Exception> failedStratgey;
+	final Predicate<Object> retryStratgey;
+	final Function<Integer, Long> waitStratgey;
+
+	private Retrier(final Predicate<Integer> stopStratgey, final Predicate<Exception> failedStratgey,
+			final Predicate<Object> retryStratgey, final Function<Integer, Long> waitStratgey) {
 		this.stopStratgey = stopStratgey;
+		this.failedStratgey = failedStratgey;
+		this.waitStratgey = waitStratgey;
+		this.retryStratgey = retryStratgey;
 	}
 
 	public static class Builder {
 		Predicate<Integer> stopStratgey = e -> false;
+		Predicate<Exception> failedStratgey = e -> true;
+		Predicate<Object> retryStratgey = obj -> false;
+		Function<Integer, Long> waitStratgey = i -> 1l;
 
 		public Builder withStopStrategy(final Predicate<Integer> stopStratgey) {
 			this.stopStratgey = stopStratgey;
 			return this;
 		}
-		
+
 		public Retrier build() {
-			return new Retrier(stopStratgey);
+			return new Retrier(stopStratgey, failedStratgey, retryStratgey, waitStratgey);
+		}
+
+		public Builder withFailedRetryStrategy(final Predicate<Exception> failedRetryStrategy) {
+			this.failedStratgey = failedRetryStrategy;
+			return this;
+		}
+
+		public Builder withResultRetryStrategy(final Predicate<Object> retryStratgey) {
+			this.retryStratgey = retryStratgey;
+			return this;
+		}
+		
+		public Builder withWaitStrategy(final Function<Integer, Long> waitStratgey) {
+			this.waitStratgey = waitStratgey;
+			return this;
 		}
 	}
 
 	public static class Strategy {
-		
-		public static Predicate<Integer> stopAfter(int maxAttempts)
-		{
+
+		public static Predicate<Integer> stopAfter(int maxAttempts) {
 			return attempts -> attempts >= maxAttempts;
 		}
 	}
@@ -46,12 +70,53 @@ public class Retrier {
 	public <T> T execute(Callable<T> callable) throws Exception {
 		int attempts = 0;
 		boolean shouldRetry;
-		//boolean attemptFailed;
+		Exception lastException = null;
+		T lastResult = null;
+		boolean attemptFailed = false;
+		boolean isInterrupted = false;
 		do {
 			attempts++;
-			callable.call();
-			shouldRetry = !stopStratgey.test(attempts);
+			lastResult = null;
+			lastException = null;
+			attemptFailed = false;
+			isInterrupted = false;
+			try {
+				lastResult = callable.call();
+				attemptFailed = retryStratgey.test(lastResult);
+			} catch (Exception e) {
+				lastException = e;
+				attemptFailed = failedStratgey.test(lastException);
+			} finally {
+				isInterrupted = Thread.interrupted() || isInterruptedException(lastException);
+				shouldRetry = attemptFailed && !isInterrupted && !stopStratgey.test(attempts);
+			}
+			if (shouldRetry) {
+				final long waitMillis = waitStratgey.apply(attempts);
+				if (waitMillis > 0) {
+					try {
+						Thread.currentThread().sleep(waitMillis);
+					} catch (InterruptedException e) {
+						isInterrupted = true;
+						shouldRetry = false;
+					}
+				}
+			}
 		} while (shouldRetry);
-		return null;
+		if (isInterrupted) {
+			Thread.currentThread().interrupt();
+		}
+		if (lastException != null) {
+			throw lastException;
+		}
+		return lastResult;
+	}
+
+	private boolean isInterruptedException(Exception lastException) {
+		Throwable lastError = lastException;
+		while (lastError != null && !InterruptedException.class.isInstance(lastError)) {
+			lastError = lastError.getCause();
+		}
+
+		return lastError != null;
 	}
 }
--- src/test/java/com/sideprojects/Retrier/RetrierTest.java
+++ src/test/java/com/sideprojects/Retrier/RetrierTest.java
@@ -1,38 +1,131 @@
 package com.sideprojects.Retrier;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.testng.Assert.fail;
+
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+import org.mockito.Mock;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.sideprojects.Retrier.Retrier.Strategy;
 
 /**
  * Unit test for simple App.
  */
-public class RetrierTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public RetrierTest( String testName )
-    {
-        super( testName );
-    }
+public class RetrierTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( RetrierTest.class );
-    }
+	private static final int MAX_ATTEMPTS = 10;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Mock
+	private Callable<Object> callable;
+	private Retrier victim;
+
+	@BeforeMethod
+	public void setUp() {
+		Thread.interrupted();
+		initMocks(this);
+		victim = createDefaultBuilder().build();
+	}
+
+	private Retrier.Builder createDefaultBuilder() {
+		return new Retrier.Builder().withStopStrategy(Strategy.stopAfter(MAX_ATTEMPTS));
+	}
+
+	private static class CustomException extends Exception {
+
+	}
+
+	private Retrier withCustomExceptionFailedStrategy() {
+		return createDefaultBuilder().withFailedRetryStrategy(CustomException.class::isInstance).build();
+	}
+
+	@Test
+	public void shouldStopFailingAfterMaxAttemptsReached() throws Exception {
+		doThrow(Exception.class).when(callable).call();
+		try {
+			victim.execute(callable);
+			fail("Did not throw an exception after last attempt failed");
+		} catch (Exception e) {
+
+		}
+		verify(callable, times(MAX_ATTEMPTS)).call();
+	}
+
+	@Test(expectedExceptions = RuntimeException.class)
+	public void shouldPropogateWithNoRetryWhenNotCustomExceptionThrown() throws Exception {
+		doThrow(RuntimeException.class).when(callable).call();
+		try {
+			victim = withCustomExceptionFailedStrategy();
+			victim.execute(callable);
+		} catch (Exception e) {
+			verify(callable, times(1)).call();
+			throw e;
+		}
+	}
+
+	@Test
+	public void shouldRetryOnRecoverableException() throws Exception {
+		willThrow(CustomException.class).willAnswer(i -> 2).given(callable).call();
+		victim = withCustomExceptionFailedStrategy();
+		Assert.assertEquals(victim.execute(callable), new Integer(2));
+		verify(callable, times(2)).call();
+	}
+
+	@Test(expectedExceptions = InterruptedException.class)
+	public void shouldRetryOnRecoverableExceptionAndStopOnInterruptedException() throws Exception {
+		willThrow(CustomException.class).willThrow(CustomException.class).willThrow(InterruptedException.class).given(callable).call();
+		victim = withCustomExceptionFailedStrategy();
+		try {
+			victim.execute(callable);
+		} finally {
+			verify(callable, times(3)).call();
+			Assert.assertTrue(Thread.currentThread().isInterrupted());
+		}
+	}
+	
+	@Test
+	public void shouldInvokeOnlyOnceWhenFirstCallDoesNotFail() throws Exception {
+		doReturn(2).when(callable).call();
+		victim = createDefaultBuilder().build();
+		victim.execute(callable);
+		verify(callable, times(1)).call();
+	}
+	
+	@Test
+	public void shouldRetryWhenUsingCustomRetry() throws Exception {
+		victim = createDefaultBuilder().withResultRetryStrategy(e -> ((Integer)e >= new Integer(2))).build();
+		doReturn(new Integer(2)).when(callable).call();
+		willAnswer(e -> 3).willAnswer( e -> 2).willAnswer(e -> 1).given(callable).call();
+		victim.execute(callable);
+		verify(callable, times(3)).call();
+	}
+	
+	@Test
+	public void shouldInvokeWaitStrategyForEveryRetry() throws Exception {
+		doThrow(CustomException.class).when(callable).call();
+		Function<Integer,Long> waitStrategy = mock(Function.class);
+		doReturn(1l).when(waitStrategy).apply(anyInt());
+		try {
+			victim = createDefaultBuilder().withFailedRetryStrategy(CustomException.class::isInstance).withWaitStrategy(waitStrategy).build();
+			victim.execute(callable);
+			fail("Did not throw an exception after last attempt failed");
+		} catch (Exception e) {
+			verify(callable, times(MAX_ATTEMPTS)).call();
+		}
+		
+		verify(callable, times(MAX_ATTEMPTS)).call();
+		verify(waitStrategy, times(MAX_ATTEMPTS-1)).apply(anyInt());
+	}
 }
